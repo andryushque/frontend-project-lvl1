a093cdf9075ff20b65ce553a4c0af2c1eab3d9dd
andryushque <andrew.ustinov23@gmail.com>
2019-11-10 15:21:24 +0300

    create gcd.js

---
+++ src/games/gcd.js
@@ -0,0 +1,42 @@
+import { cons, car, cdr } from '@hexlet/pairs';
+import brainGamesEngine from '../gameEngine';
+
+
+// игра #3 "Наибольший общий делитель"
+// правила игры
+const gameRules = 'Find the greatest common divisor of given numbers.';
+
+// описание одного раунда игры
+const gameRound = () => {
+  // верхняя и нижняя границы генератора случайных чисел
+  const min = 1;
+  const max = 100;
+  // сам генератор - генерируем два числа
+  const number1 = Math.floor(Math.random() * (max - min)) + min;
+  const number2 = Math.floor(Math.random() * (max - min)) + min;
+
+  // вопрос, выводимый на экран
+  const question = `${number1} ${number2}`;
+
+  // функция вычисления НОД двух чисел (по алгоритму Евклида)
+  const gcd = (a, b) => {
+    if (b === 0) {
+      return a;
+    }
+    return gcd(b, a % b);
+  };
+  // правильный ответ на вопрос
+  const answer = gcd(number1, number2);
+  // переводим правильный ответ в строку, так как введенный ответ тоже строка
+  const correctAnswer = String(answer);
+
+  // пара вопрос-правильный ответ
+  return cons(question, correctAnswer);
+};
+
+// из каждого раунда получаем вопрос и правильный ответ на него
+const gameQuestion = (singleRound) => car(singleRound);
+const gameAnswer = (singleRound) => cdr(singleRound);
+
+// экспорт по умолчанию (импорт в исполняемом файле игры)
+export default () => brainGamesEngine(gameRules, gameRound, gameQuestion, gameAnswer);
