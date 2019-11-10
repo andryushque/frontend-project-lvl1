506c353f0c392c6721b2e5da1ba126a9eb295f64
andryushque <andrew.ustinov23@gmail.com>
2019-11-10 13:45:16 +0300

    update even.js
    add gameEngine to the game

--- src/games/even.js
+++ src/games/even.js
@@ -1,48 +1,30 @@
-import readlineSync from 'readline-sync';
+import { cons, car, cdr } from '@hexlet/pairs';
+import brainGamesEngine from '../gameEngine';
+
 
 // игра #1 "Проверка на четность"
-const brainGamesEven = () => {
-  // приветствие
-  console.log('Welcome to the Brain Games!');
-  // правила игры
-  const gameRules = "Answer 'yes' if the number is even, otherwise answer 'no'.";
-  console.log(gameRules);
-  console.log();
-  // ввод имени игрока
-  const name = readlineSync.question('Please, enter your name here: ');
-  console.log(`Hello, ${name}!!!`);
-  console.log();
+// правила игры
+const gameRules = "Answer 'yes' if the number is even, otherwise answer 'no'.";
 
-  // количество раундов игры
-  const roundCount = 3;
+// функция проверки четности числа
+const isEven = (number) => number % 2 === 0;
 
-  // функция проверки четности числа
-  const isEven = (number) => number % 2 === 0;
+// описание одного раунда игры
+const gameRound = () => {
+  // верхняя и нижняя границы генератора случайных чисел
+  const min = 1;
+  const max = 256;
+  // вопрос, выводимый на экран
+  const question = Math.floor(Math.random() * (max - min)) + min;
+  // правильный ответ на вопрос
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
+  // пара вопрос-правильный ответ
+  return cons(question, correctAnswer);
+};
 
-  // логика самой игры
-  for (let i = 0; i < roundCount; i += 1) {
-    // верхняя и нижняя границы генератора случайных чисел
-    const min = 1;
-    const max = 99;
-    // вопрос, выводимый на экран
-    const question = Math.floor(Math.random() * (max - min)) + min;
-    // правильный ответ на вопрос
-    const correctAnswer = isEven(question) ? 'yes' : 'no';
+// из каждого раунда получаем вопрос и правильный ответ на него
+const gameQuestion = (singleRound) => car(singleRound);
+const gameAnswer = (singleRound) => cdr(singleRound);
 
-    // выводим вопрос
-    console.log(`Question: ${question}`);
-    // на экран выводится просьба ввести ответ
-    const yourAnswer = readlineSync.question('Your answer is: ');
-    // сравниваем введенный ответ с правильным
-    if (yourAnswer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${yourAnswer}' is wrong answer ;((. Correct answer was '${correctAnswer}' =)`);
-      console.log(`Let's try again, ${name}!`);
-      return;
-    }
-  } // цикл for
-  console.log(`Congratulations, ${name}!`);
-}; // brainGamesEven
-
-export default brainGamesEven;
+// экспорт по умолчанию (импорт в исполняемом файле игры)
+export default () => brainGamesEngine(gameRules, gameRound, gameQuestion, gameAnswer);
